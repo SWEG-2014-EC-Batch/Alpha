08b94bac64e4c7c22a1a06493a0df10daf7fda38
Mahider Jemere <124486265+Mahi-gitlex@users.noreply.github.com>
2023-07-29 14:27:11 +0300

    Update Main_code.cpp

--- FoP-II/Mahider Jemere/Home Work/File Management/Question 8/Main_code.cpp
+++ FoP-II/Mahider Jemere/Home Work/File Management/Question 8/Main_code.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
-#include <fstream>
+#include<fstream>
 using namespace std;
 
 int main()
 {
-    ifstream lw_case("Sheet5Ex5.cpp", ios::in);
-    if (!lw_case.is_open()) {
-        cout << "Couldn't open the file";
-        return 0;
-    }
-
-    int occurrences[26] = {0};  // Array to store the occurrences of each character
     char c;
+    int i=97,counter;
+    ifstream file("Sheet5Ex5.cpp",ios::in);
+    if(!file.is_open()){
+        cout<<"Couldn't open a file";
+    }
+    else{
+       cout<<"Character            Occurrence"<<endl;
+       while(i<123){
+        counter=0;
+        while(!file.eof()){
+            file.get(c);
 
-    while (lw_case.get(c)) {
-        if (c >= 'a' && c <= 'z') {
-            occurrences[c - 'a']++;  // Increment the occurrence count for the specific character
+            if(i==(int)c){
+                counter=counter+1;
+            }
+
         }
-    }
+        file.close();
+        cout<<(char)i<<"                          "<<counter<<endl;
+        i=i+1;
+        file.open("Sheet5Ex5.cpp",ios::in);
+       }
 
-    cout << "CHARACTER\t\t\tOCCURRENCES\n";
-    for (int i = 0; i < 26; i++) {
-        cout << static_cast<char>('a' + i) << "\t\t\t\t" << occurrences[i] << "\n";
     }
-
-    lw_case.close();
+    file.close();
     return 0;
 }
