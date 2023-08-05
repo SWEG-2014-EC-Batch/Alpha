ebe6a284b72f90291dcef468fb518b066073177a
Mahider Jemere <124486265+Mahi-gitlex@users.noreply.github.com>
2023-08-05 13:11:31 +0300

    Update Main_code.cpp

--- FoP-II/Mahider Jemere/Home Work/File Management/Question 4/Main_code.cpp
+++ FoP-II/Mahider Jemere/Home Work/File Management/Question 4/Main_code.cpp
@@ -1,137 +1,141 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-
+#include<fstream>
+#include<algorithm>
 using namespace std;
 
-struct Employee {
-    int id;
+struct emp_info{
+    string name;
+    int id_no, experience, hr_worked;
+    double hourly_wage,hr_payment;
     char sex;
-    double hourlyWage;
-    int yearsWithCompany;
-};
 
-void storeEmployeeRecords(const string& filename) {
-    ofstream file(filename);
-    if (!file.is_open()) {
-        cout << "Couldn't open the file" << endl;
-        return;
+}emp1[3],emp2[3];
+
+
+void str_data(){
+    cin.ignore();
+    for(int i=0;i<3;i++){
+    cout<<"Please enter the name of the employee: ";
+    getline(cin,emp1[i].name);
+    cout<<"Please enter the sex of the employee: ";
+    cin>>emp1[i].sex;
+    cout<<"Please enter his/her id number: ";
+    cin>>emp1[i].id_no;
+    cout<<"Please enter his/her experience: ";
+    cin>>emp1[i].experience;
+    cout<<"Please enter his/her hourly wage: ";
+    cin>>emp1[i].hourly_wage;
+    cin.ignore();}
+}
+
+void writedata(){
+    fstream outinf("employee.dat",ios::out | ios::binary | ios::app);
+    str_data();
+    int least,m,counter[3],arr_size,c;
+    for(int j=0;j<3;j++){
+        counter[j]=emp1[j].id_no;
     }
-
-    Employee employees[10];
-
-    cout << "Enter information for 10 employees:" << endl;
-    for (int i = 0; i < 10; i++) {
-        Employee emp;
-
-        cout << "Employee " << i + 1 << ":" << endl;
-        cout << "ID No.: ";
-        cin >> emp.id;
-        cout << "Sex(M/F): ";
-        cin >> emp.sex;
-        cout << "Hourly Wage: ";
-        cin >> emp.hourlyWage;
-        cout << "Years with the Company: ";
-        cin >> emp.yearsWithCompany;
-
-        employees[i] = emp;
-    }
-
-    // Sort the employees based on ID number using bubble sort algorithm
-    for (int i = 0; i < 10 - 1; i++) {
-        for (int j = 0; j < 10 - i - 1; j++) {
-            if (employees[j].id > employees[j + 1].id) {
-                Employee temp = employees[j];
-                employees[j] = employees[j + 1];
-                employees[j + 1] = temp;
-            }
+    arr_size=sizeof(counter)/sizeof(int);
+    sort(counter, counter + arr_size);
+   for(int j=0;j<3;j++){
+    for(int k=0;k<3;k++){
+        if(counter[j]==emp1[k].id_no){
+            c=k;
         }
     }
+    outinf.write((char *)&emp1[c],sizeof(emp1[c]));
+    outinf.close();
+    outinf.open("employee.dat",ios::out | ios::binary | ios::app);
+   }
+   outinf.close();
 
-    for (int i = 0; i < 10; i++) {
-        file << employees[i].id << " " << employees[i].sex << " " << employees[i].hourlyWage << " " << employees[i].yearsWithCompany << endl;
-    }
 
-    file.close();
 }
 
-void calculateTotalPay(const string& filename) {
-    ifstream file(filename);
-    if (!file.is_open()) {
-        cout << "Couldn't open the file" << endl;
-        return;
+void readdata(){
+    fstream empinf("employee.dat",ios::in | ios::binary);
+    for(int h=0;h<3;h++){
+        empinf.read((char *)&emp2[h], sizeof(emp2[h]));
+        char c='n';
+        do{
+            cout<<"Please enter the number of hours "<<emp2[h].name<<" worked this month"<<endl;
+            cin>>emp2[h].hr_worked;
+            cout<<"The total pay for "<<emp2[h].name<<" is "<<emp2[h].hourly_wage*emp2[h].hr_worked<<endl;
+            cout<<"Do you want to calculate for the next month?(y/n) ";
+            cin>>c;
+        }while(c=='y');
     }
+    empinf.close();
+}
 
-    double hoursWorked;
+void updatedata(){
+    fstream empinf("employee.dat",ios::in | ios::binary);
+    fstream outinf("employee.dat",ios::out | ios::binary);
+    int choice;
+    char c='n';
+    for(int i=0;i<3;i++){
+        empinf.read((char *)&emp2[i], sizeof(emp2[i]));
+        do{
+        cout<<"Do you want to update the(for "<<emp2[i].name<<")\n1. Hourly wage\n2. Experience\n3. Other numbers to exit? "<<endl;
+        cin>>choice;
 
-    cout << "Enter the number of hours worked for each employee:" << endl;
+        if(choice==1){
+            cout<<"Please enter new hourly wage(the previous value was "<<emp2[i].hourly_wage<<") for "<<emp2[i].name<<endl;
+            cin>>emp2[i].hourly_wage;
 
-    while (true) {
-        Employee emp;
-        file >> emp.id >> emp.sex >> emp.hourlyWage >> emp.yearsWithCompany;
+        }
+        else if(choice==2){
+            cout<<"Please enter new hourly wage(the previous value was "<<emp2[i].experience<<") for "<<emp2[i].name<<endl;
+            cin>>emp2[i].experience;
 
-        if (file.eof())
+        }
+        else{
+
             break;
-
-        cout << "Employee ID: " << emp.id << endl;
-        cout << "Number of hours worked: ";
-        cin >> hoursWorked;
-
-        double totalPay = hoursWorked * emp.hourlyWage;
-        cout << "Total pay for the month: $" << totalPay << endl;
+        }
+        cout<<"Do you want to continue the experience or hour wage update for "<<emp2[i].name<<"?(y/n) "<<endl;
+        cin>>c;
+    }while(c=='y');
+    outinf.write((char *)&emp2[i], sizeof(emp2[i]));
+    outinf.close();
+    outinf.open("employee.dat",ios::out | ios::binary | ios::app);
+    }
+    empinf.close();
     }
 
-    file.close();
+void printdata(){
+     fstream empinf("employee.dat",ios::in | ios::binary);
+    for(int h=0;h<3;h++){
+        empinf.read((char *)&emp2[h], sizeof(emp2[h]));
+        cout<<"Name: "<<emp2[h].name<<endl;
+        cout<<"Sex: "<<emp2[h].sex<<endl;
+        cout<<"ID Number: "<<emp2[h].id_no<<endl;
+        cout<<"Experience: "<<emp2[h].experience<<endl;
+        cout<<"Hourly Wage: "<<emp2[h].hourly_wage<<endl;
+}
+empinf.close();
 }
 
-void updateEmployeeData(const string& filename) {
-    fstream file(filename, ios::in | ios::out);
-    if (!file.is_open()) {
-        cout << "Couldn't open the file" << endl;
-        return;
-    }
-
-    Employee employees[10];
-
-    int i = 0;
-    while (true) {
-        Employee emp;
-        file >> emp.id >> emp.sex >> emp.hourlyWage >> emp.yearsWithCompany;
-
-        if (file.eof())
-            break;
-
-        employees[i] = emp;
-        i++;
-    }
-
-    file.clear();
-    file.seekp(0);
-
-    for (int i = 0; i < 10; i++) {
-        cout << "Employee ID: " << employees[i].id << endl;
-        cout << "Enter updated hourly wage: ";
-        cin >> employees[i].hourlyWage;
-        cout << "Enter updated years with the company: ";
-        cin >> employees[i].yearsWithCompany;
-
-        file << employees[i].id << " " << employees[i].sex << " " << employees[i].hourlyWage << " " << employees[i].yearsWithCompany << endl;
-    }
-
-    file.close();
-}
-
-int main() {
-    const string filename = "employee_records.txt";
-
-    // Store employee records in a file
-    storeEmployeeRecords(filename);
-
-    // Calculate total pay for each employee
-    calculateTotalPay(filename);
-
-    // Update employee data and create a new file
-    updateEmployeeData(filename);
-
+int main()
+{
+        int choice;
+        char c='y';
+        do{
+        cout<<"Please choose what you want to do with the file\n1. Write to the file\n2. Read from the file\n3. Update the file\n4. Print the data in the file "<<endl;
+        cin>>choice;
+        if(choice==1){
+        writedata();
+        }
+        else if(choice==2){
+        readdata();}
+        else if(choice==3){
+            updatedata();
+        }
+        else if(choice==4){
+            printdata();
+        }
+        cout<<"Do you want the program to continue?(y/n) ";
+        cin>>c;
+    }while(c=='y');
     return 0;
 }
